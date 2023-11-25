c825e7c8a4261766c6b37cf8b7164eb834d095a4
rebecabertolim <rebecalopes@alunos.utfpr.edu.br>
2023-11-25 01:02:44 -0300

    estruturando corpo'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageLayoutComponent } from './page-layout/page-layout.component';
+import { CorpoComponent } from './corpo/corpo.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
     path: '',
     component: PageLayoutComponent,
-    children: [],
+    children: [{ path: 'home', component: CorpoComponent }],
   },
 ];
 
